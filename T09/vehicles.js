777b53cd189491cf6f73f33ae0a5fa96fc9a3e3a
Kamil Synowiec <1935002@brunel.ac.uk>
2022-11-15 11:39:46 +0000

    Create vehicles.js

---
+++ T09/vehicles.js
@@ -0,0 +1,7 @@
+/*this program prints out all cars from the array one by one after the sentence "I would love to 
+drive a " */
+let vehicles=["Bugatti", "Pagani", "Lamborghini","Bentley", "Ferrari"];
+
+for(let i=0; i<5; i++){
+    console.log("I would love to drive a "+vehicles[i]);
+}
