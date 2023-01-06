44245e3611676d692d5e4ad8da2f63e2054a1f1c
Kamil Synowiec <1935002@brunel.ac.uk>
2023-01-06 16:34:31 +0000

    Create citiesAPI.js

---
+++ projects/citiesAPI.js
@@ -0,0 +1,46 @@
+//variables used to store position of the city to use for finding out temperature from the second api
+let longitude;
+let latitude;
+
+
+const optionsCity = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c24132acdemsh8f13cfa66d7c6b8p1d25c7jsnec2d75b1a2fd',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+const optionsWeather = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'c24132acdemsh8f13cfa66d7c6b8p1d25c7jsnec2d75b1a2fd',
+        'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
+    }
+};
+
+fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/cities/Q34647', optionsCity)   //Q34647 - Johannesburg wikidata ID used to find the city
+	.then(response => response.json())
+	.then(response => {
+
+        //display details about the city: name, population and elevation
+        console.log("city: "+response.data.city);
+        console.log("population: "+response.data.population+"\n");
+        console.log("elevation: "+response.data.elevationMeters+"m");   
+
+        /*saving location details of the city into variables to use in weather API */
+        longitude=response.data.longitude;  
+        latitude=response.data.latitude;
+    })
+	.catch(err => console.error(err));    
+
+
+
+    
+fetch('https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=28.041638888&lat=-26.204361111', optionsWeather)
+    .then(response => response.json())
+    .then(response => {
+        console.log("Current temperature: "+response.data[0].temp) //displaying current remperature
+
+    })
+    .catch(err => console.error(err));
