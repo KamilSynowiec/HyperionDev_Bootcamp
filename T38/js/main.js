32e35c941df52e0329087d8271772abde7a462cb
Kamil Synowiec <1935002@brunel.ac.uk>
2023-01-14 01:12:53 +0100

    Create main.js

---
+++ T38/js/main.js
@@ -0,0 +1,46 @@
+const groceryItems =["Apple", "Eggs", "Milk", "Bread"];  
+
+
+/*this function adds each item to the unordered list and displays it.
+It also turns red 2 items to indicate they are sold and adds strikethrough line to them*/
+function displayShopping(groceryItems){
+    
+
+    let list=document.getElementById("itemList");  //getting our list
+
+    groceryItems.forEach(function(item){   //for each loop is used to add all items from the grocery list
+
+        let newItem=document.createElement("li");   //creating new <li>
+        newItem.textContent=item; //changing content of the <li>
+
+        let spanElement=document.createElement("span");
+        
+        spanElement.textContent=' \u00D7';
+        
+        newItem.appendChild(spanElement);
+
+        list.appendChild(newItem);   //adding the newly created <li> to the original unordered list <ul>
+    });
+
+
+    //styling 2 items: Eggs and Bread to indicate they are sold
+    let secondSold=document.querySelector("ul > li:nth-child(2)");
+    secondSold.style.color="red";
+    secondSold.style.setProperty("text-decoration", "line-through");
+    let forthSold=document.querySelector("ul > li:nth-child(4)");
+    forthSold.style.color="red";
+    forthSold.style.setProperty("text-decoration", "line-through");
+
+
+    //deleteItem();
+}
+
+
+displayShopping(groceryItems);
+
+function updateList(){
+    let input=document.getElementById('input');
+    groceryItems.push(input.value);
+
+    console.log(groceryItems);
+}
