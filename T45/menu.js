eab719f7b31883e80a7690e69365856665c38b05
Kamil Synowiec <1935002@brunel.ac.uk>
2023-02-19 00:45:26 +0100

    Create menu.js

---
+++ T45/menu.js
@@ -0,0 +1,11 @@
+let item1, item2, item3;
+
+//assigning favourite foods to strings
+item1="pizza";  
+item2="spaghetti";
+item3="waffle";
+
+//displaying the items
+console.log("1. "+item1);
+console.log("2. "+item2);
+console.log("3. "+item3);
