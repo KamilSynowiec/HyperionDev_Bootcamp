e257e3f035a25eb6bc22e781afa7f2008be6cd25
Kamil Synowiec <1935002@brunel.ac.uk>
2023-02-20 20:07:51 +0100

    Create Welcome.js

---
+++ T49/practice/Components/Welcome.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Welcome(props){
+    const displayAge=()=>{
+        alert(props.age);
+    };
+
+
+    return(
+        <div>
+            <h1>Hello World, {props.name}</h1>
+            <button onClick={displayAge}>Display The User's Age</button>
+        </div>
+    );
+}
+
+export default Welcome;
