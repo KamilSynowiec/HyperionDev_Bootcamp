edc36a8651b3c78efe5fe5f6bd595a6df76322f4
Kamil Synowiec <1935002@brunel.ac.uk>
2023-02-20 14:22:02 +0100

    Create App.js

---
+++ T48/practice/App.js
@@ -0,0 +1,50 @@
+import {Routes, Route, Link} from "react-router-dom";
+
+function Landing(){
+    return(
+        <div>
+            <main>
+                <h2>Welcome</h2>
+            </main>
+            <nav>
+                <Link to="/cart">Cart</Link>
+            </nav>
+        </div>
+    );
+}
+
+function ShoppingCart(){
+    return(
+        <div>
+            <main>
+                <h2>Shopping Cart</h2>
+            </main>
+            <nav>
+                <Link to="/">Home</Link>
+            </nav>
+        </div>
+    );
+}
+
+function Header(){
+    return(
+        <h1>
+            <u>This is the Header</u>
+        </h1>
+    );
+}
+
+
+function App(){
+    return(
+        <div className="App">
+            <Header/>
+            <Routes>
+                <Route exact path="/" element={<Landing />}/>
+                <Route path="/cart" element={<ShoppingCart />}/>
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
