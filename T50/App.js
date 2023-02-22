c66d69ee5d8d2041d2cbfe8db0915ff566e9c9b7
Kamil Synowiec <1935002@brunel.ac.uk>
2023-02-22 21:24:11 +0100

    Update App.js

--- T50/App.js
+++ T50/App.js
@@ -1,49 +1,39 @@
-import './index.css';
-//importing these functions to select required slices of state
-import { useSelector, useDispatch } from "react-redux";
-//importing the action creators from counter reducer
-import {decrement, increment, incrementByAmount} from "./store/counter";
-import {useState} from "react";
+import {Routes, Route} from "react-router-dom"; 
+import "./styles.css"; //stylesheet
+import React from "react";
 
-function App() {
+//Navigation
+import NavItems from "./components/NavItems.js";
+//Header
+import Header from "./components/Header.js";
+//Landing Page
+import LandingPage from "./components/LandingPage.js";
+//Shopping component
+import Shopping from "./components/Shopping.js";
+//Calculator
+import Calculator from "./components/Calculator.js";
+//legal
+import Legal from "./components/Legal.js";
+//Profile
+import Profile from "./components/Profile.js";
+  
 
-  //useSelector function allows to find the required sliced
-  const count = useSelector((state)=>state.counter.value);
-
-  //useDispatch allows to dispatch all actions to the reducer
-  const dispatch = useDispatch();
-
-  const [userInput, setUserInput] = useState(0);
-
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    dispatch(incrementByAmount(Number(userInput)));
-    setUserInput(0)
-  };
-
-
-  return (
-    <div>
+  function App() {
+    return (
       <div className="App">
-        <h1>{count}</h1>  {/*the variable count is displayed that was initiated by using useSelector */}
+        <NavItems/>
+        <Header isLoggedIn={true} name="Michael"/>
+        <Routes>
+            <Route exact path="/" element={<LandingPage/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+            <Route path="/shopping" element={<Shopping/>}/>
+            <Route path="/legal" element={<Legal/>}/>
+            <Route path="/calc" element={<Calculator/>}/>
+        </Routes>
+    
+       
       </div>
-      <div className="Buttons">
-        {/*when the button is clicked the state will incremenet */}
-        <button onClick={()=> dispatch(increment())}>Increment</button>
-        {/*when the button is clicked the state will decremenet */}
-        <button onClick={()=> dispatch(decrement())}>Decrement</button>
-      </div>
-      <form className="Form" onClick={handleSubmit}>
-        <label>
-          Value:
-          <input type="text" name="value" onChange={(e) => setUserInput(e.target.value)} value={userInput}/>
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-
-    </div>
-  );
-}
+    );
+  }
 
 export default App;
