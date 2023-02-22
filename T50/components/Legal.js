50080c28dbca67c4695920e2945a5ea720c2cdf1
Kamil Synowiec <1935002@brunel.ac.uk>
2023-02-22 21:22:37 +0100

    Create Legal.js

---
+++ T50/components/Legal.js
@@ -0,0 +1,16 @@
+//legal page 
+function Legal(){
+    return(
+        <div>
+            <h2>Legal page</h2>
+            <h3>Terms and Conditions</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <h3>Return policy</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <h3>Shipping and payment info</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+    );
+}
+
+export default Legal;
